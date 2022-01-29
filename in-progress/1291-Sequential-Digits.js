42efabf5d03dec2c62a376e229de72efb40b1466
Martin Halas <marhaldev@gmail.com>
2022-01-29 15:24:22 -0500

    Added skeleton for 1291

---
+++ in-progress/1291-Sequential-Digits.js
@@ -0,0 +1,8 @@
+
+const sequentialDigits = (low, high) => {
+    
+};
+
+console.log(sequentialDigits(100,300)) // [123,234]
+console.log(sequentialDigits(1000,13000)) // [1234,2345,3456,4567,5678,6789,12345]
+
