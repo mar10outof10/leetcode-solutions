180c1518cec9358ababcfb3d2de64e1116b2e74d
mar10outof10 <78447684+mar10outof10@users.noreply.github.com>
2023-10-06 13:46:58 -0400

    Added skeleton for two-sum

---
+++ in-progress/0001-two-sum-2.js
@@ -0,0 +1,8 @@
+const twoSum =  (nums, target) => {
+
+};
+
+// test cases
+console.log(twoSum([2,7,11,15], 9)); // [0, 1];
+console.log(twoSum([3,2,4], 6)); // [1, 2];
+console.log(twoSum([3,3], 6)); // [0, 1];
