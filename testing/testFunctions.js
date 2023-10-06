3532247e65f5504e6679bf8982d6d03fa7eecc7c
mar10outof10 <78447684+mar10outof10@users.noreply.github.com>
2023-10-06 14:52:40 -0400

    Setup skeleton for generateSolutionsObject() and docstrings

--- testing/testFunctions.js
+++ testing/testFunctions.js
@@ -1,5 +1,18 @@
-const testSolution = (solutions) => {
+/**
+ * Generates solutions object to be used by testSolution function
+ * @param {Object} solutions 
+ */
+const generateSolutionsObject = (solutions) => {
+  return null;
+}
 
+/**
+ * Assertion function to compare testFunction generated answers with known solutions
+ * @param {Function} testFunction 
+ * @param {Object} solutions 
+*/
+const testSolution = (testFunction, solutions) => {
+  return null;
 };
 
-export { testSolution };
+export { generateSolutionsObject, testSolution };
