1f3b24e7db47f69b8a474add6d6072c3a2470522
mar10outof10 <78447684+mar10outof10@users.noreply.github.com>
2023-10-11 16:58:37 -0400

    Finished creating testSolutions function

--- testing/testFunctions.js
+++ testing/testFunctions.js
@@ -1,18 +1,30 @@
-/**
- * Generates solutions object to be used by testSolution function
- * @param {Object} solutions 
- */
-const generateSolutionsObject = (solutions) => {
-  return null;
-}
+import lodash from 'lodash';
+const { isEqual } = lodash;
 
 /**
  * Assertion function to compare testFunction generated answers with known solutions
  * @param {Function} testFunction 
- * @param {Object} solutions 
+ * @param {Array} solutions 
 */
 const testSolution = (testFunction, solutions) => {
+
+  for (const solution of solutions) {
+    const { answer, ...params } = solution;
+    const testAnswer = testFunction({...params});
+
+    if (isEqual(answer, testAnswer)) {
+      console.log(`
+        The correct value, ${answer}, is properly returned by the provided function.
+      `)
+    } else {
+      console.log(`
+        The correct value, ${answer}, is not returned by the provided function.
+        The function returns, ${testAnswer}, instead.
+      `)
+    }
+  }
+
   return null;
 };
 
-export { generateSolutionsObject, testSolution };
+export { testSolution };
