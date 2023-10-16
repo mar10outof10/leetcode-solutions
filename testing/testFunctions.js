3e77bd229d6788d6ccd79b073a13ed985106c40b
mar10outof10 <78447684+mar10outof10@users.noreply.github.com>
2023-10-16 18:55:51 -0400

    Updated testFunctions to include tests for arrays independant of sorting. Updated old testSolutions to be testSolutionsStrictg

--- testing/testFunctions.js
+++ testing/testFunctions.js
@@ -1,12 +1,13 @@
 import lodash from 'lodash';
-const { isEqual } = lodash;
+const { isEqual, sortBy } = lodash;
 
 /**
- * Assertion function to compare testFunction generated answers with known solutions
+ * Assertion function to strictly compare testFunction generated answers with known solutions
+ * Will display success only if answers are strictly equal
  * @param {Function} testFunction 
  * @param {Array} solutions 
 */
-const testSolution = (testFunction, solutions) => {
+const testSolutionStrict = (testFunction, solutions) => {
 
   let success = true;
 
@@ -16,9 +17,9 @@ const testSolution = (testFunction, solutions) => {
     const testAnswer = testFunction(...Object.values(params));
 
     if (isEqual(answer, testAnswer)) {
-      console.log(`\n \u2705 The correct value, ${answer}, is returned by the provided function.`);
+      console.log(`\n \u2705 The correct answer, ${answer}, is returned by the provided function.`);
     } else {
-      console.log(`\n \u274C The correct value, ${answer}, is not returned by the provided function.\n    The function returns, ${testAnswer}, instead.`);
+      console.log(`\n \u274C The correct answer, ${answer}, is not returned by the provided function.\n    The function returns, ${testAnswer}.`);
       success = false;
     }
   };
@@ -28,4 +29,32 @@ const testSolution = (testFunction, solutions) => {
   return null;
 };
 
-export { testSolution };
+/**
+ * Assertion function to compare testFunction generated array answers with known solutions
+ * Will display success if array contents are identical regardless of sorting order
+ * @param {Function} testFunction 
+ * @param {Array} solutions 
+*/
+const testSolutionUnorderedArray = (testFunction, solutions) => {
+
+  let success = true;
+
+  for (const solution of solutions) {
+
+    const { answer, ...params } = solution;
+    const testAnswer = testFunction(...Object.values(params));
+
+    if (isEqual(sortBy(answer), sortBy(testAnswer))) {
+      console.log(`\n \u2705 The correct answer, ${testAnswer}, is returned by the provided function.`);
+    } else {
+      console.log(`\n \u274C The correct answer, ${answer}, is not returned by the provided function.\n    The function returns, ${testAnswer}.`);
+      success = false;
+    }
+  };
+
+  console.log(`\n ${success ? `\u{1F600}` : `\u{1F621}` } Unit tests ${success ? `sucessful` : `failed`}\n`);
+
+  return null;
+};
+
+export { testSolutionStrict, testSolutionUnorderedArray };
