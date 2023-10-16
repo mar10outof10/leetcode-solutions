2868bacab34e7db4b07d9b1b52041cabab1f0115
mar10outof10 <78447684+mar10outof10@users.noreply.github.com>
2023-10-16 19:29:45 -0400

    Added testMultipleSolutionsStrict function

--- testing/testFunctions.js
+++ testing/testFunctions.js
@@ -30,6 +30,34 @@ const testSolutionsStrict = (testFunction, solutions) => {
 };
 
 /**
+ * Assertion function to compare testFunction generated answer with known solutions
+ * Will display success if answer matches one of the solutions in the answers array provided
+ * @param {Function} testFunction 
+ * @param {Array} solutions 
+*/
+const testMultipleSolutionsStrict = (testFunction, solutions) =>{
+
+  let success = true;
+
+  for (const solution of solutions) {
+
+    const { answers, ...params } = solution;
+    const testAnswer = testFunction(...Object.values(params));
+
+    if (answers.includes(testAnswer)) {
+      console.log(`\n \u2705 A correct answer, ${answer}, is returned by the provided function.`);
+    } else {
+      console.log(`\n \u274C A correct answer is not returned by the provided function.\n    The function returns, ${testAnswer}.\n   The correct answer(s) are '${[...answers].join(`' | '`)}'`);
+      success = false;
+    }
+  };
+
+  console.log(`\n ${success ? `\u{1F600}` : `\u{1F621}` } Unit tests ${success ? `sucessful` : `failed`}\n`);
+
+  return null;
+};
+
+/**
  * Assertion function to compare testFunction generated array answers with known solutions
  * Will display success if array contents are identical regardless of sorting order
  * @param {Function} testFunction 
@@ -57,4 +85,4 @@ const testSolutionsUnorderedArray = (testFunction, solutions) => {
   return null;
 };
 
-export { testSolutionsStrict, testSolutionsUnorderedArray };
+export { testSolutionsStrict, testMultipleSolutionsStrict, testSolutionsUnorderedArray };
