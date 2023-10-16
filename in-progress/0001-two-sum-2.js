0551865f3cdcff9b9118e4a8a61111a0c09ff903
mar10outof10 <78447684+mar10outof10@users.noreply.github.com>
2023-10-16 16:07:31 -0400

    Modified 0001 to include testSolution import and example solutions

--- in-progress/0001-two-sum-2.js
+++ in-progress/0001-two-sum-2.js
@@ -1,8 +1,26 @@
-const twoSum =  (nums, target) => {
+import { testSolution } from '#testing/testFunctions.js';
 
+const twoSum =  (nums, target) => {
+  return null;
 };
 
 // test cases
-console.log(twoSum([2,7,11,15], 9)); // [0, 1];
-console.log(twoSum([3,2,4], 6)); // [1, 2];
-console.log(twoSum([3,3], 6)); // [0, 1];
+const solutions = [
+  {
+    'nums': [2,7,11,15],
+    'target': 9,
+    'answer': [0,1],
+  },
+  {
+    'nums': [3,2,4],
+    'target': 6,
+    'answer': [1,2],
+  },
+  {
+    'nums': [3,3],
+    'target': 6,
+    'answer': [0,1],
+  },
+];
+
+testSolution(twoSum, solutions);
